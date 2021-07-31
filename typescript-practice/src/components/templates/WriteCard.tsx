0a4ad327bafdc8042976a68c0d1c0ac8636295e6
Hyoin-kim <hi980506@naver.com>
2021-08-01 02:34:00 +0900

    Feat : challengeWrite 임시저장

--- typescript-practice/src/components/templates/WriteCard.tsx
+++ typescript-practice/src/components/templates/WriteCard.tsx
@@ -1,17 +1,18 @@
-// import { writeForm } from 'apis';
+import { writeForm } from 'apis';
+import {
+  CharacterBlackIcon,
+  CharacterColor1Icon,
+  CharacterColor2Icon,
+  CharacterColor3Icon,
+  MoreClickedIcon,
+} from 'assets/images';
 import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
-// import { useRecoilState } from 'recoil';
-// import { userStatusState } from 'stores/user';
-import Styled from 'styled-components';
-import CharacterBlack from '../../assets/images/character_black.svg';
-import CharacterColor1 from '../../assets/images/character_color1.svg';
-import CharacterColor2 from '../../assets/images/character_color2.svg';
-import CharacterColor3 from '../../assets/images/character_color3.svg';
-import MoreIcon from '../../../assets/images/moreIcon.svg';
-// import { interestList } from '../../../resources/string';
-import Button from '../atoms/Button';
-import Modal from '../atoms/Modal/index';
+import { useRecoilState } from 'recoil';
+import { userStatusState } from 'stores/user';
+import { interestList } from '../../../resources/string';
+import { Button, Modal } from '../../atoms';
+import { Header, SWriteCard } from './style';
 
 type selectedStyle = {
   backgroundColor: string;
@@ -60,7 +61,7 @@ type MyFormProps = {
 
 function WriteCard({ onChangeForm }: MyFormProps) {
   const history = useHistory();
-  // const [userStatusData, setUserStatusData] = useRecoilState(userStatusState);
+  const [userStatusData, setUserStatusData] = useRecoilState(userStatusState);
   const [isBadgeModal, setIsBadgeModal] = useState(false); //뱃지 모달
   const [countProgressBar, setCountProgressBar] = useState(0); //프로그래스바
   const maxByte = 1000; //최대 1000바이트
@@ -102,10 +103,10 @@ function WriteCard({ onChangeForm }: MyFormProps) {
       learn: description3,
       interest: selectedInterest,
     };
-    // if (userStatusData) {
-    //   const data = await writeForm(writeData, userStatusData.token);
-    //   history.goBack();
-    // }
+    if (userStatusData) {
+      const data = await writeForm(writeData, userStatusData.token);
+      history.goBack();
+    }
   };
 
   const progressBarState = () => {
@@ -212,11 +213,11 @@ function WriteCard({ onChangeForm }: MyFormProps) {
   return (
     <>
       <SWriteCard>
-        <div className="header">Learn Myself</div>
+        <Header className="header">Learn Myself {indextoName(userStatusData?.progressGeneration)}</Header>
         {countProgressBar === 0 ? (
           <div className="character">
             <div className="character__color-step0">
-              <img className="character__black" src={CharacterBlack} alt="" />
+              <img className="character__black" src={CharacterBlackIcon} alt="" />
               <div className="character__message">당신의 오늘을 알고싶어요!</div>
             </div>
             <div className="bar">
@@ -230,7 +231,7 @@ function WriteCard({ onChangeForm }: MyFormProps) {
         {countProgressBar === 1 ? (
           <div className="character">
             <div className="character__color-step1">
-              <img className="character__detail-step1" src={CharacterColor1} alt="" />
+              <img className="character__detail-step1" src={CharacterColor1Icon} alt="" />
               <div className="character__message">오호라! 오늘 이런 일이 있었군요!</div>
             </div>
             <div className="bar">
@@ -243,7 +244,7 @@ function WriteCard({ onChangeForm }: MyFormProps) {
         {countProgressBar === 2 ? (
           <div className="character">
             <div className="character__color-step2">
-              <img className="character__detail-step2" src={CharacterColor2} alt="" />
+              <img className="character__detail-step2" src={CharacterColor2Icon} alt="" />
               <div className="character__message">우와! 내일의 당신은 더 행복할거에요 :)</div>
             </div>
             <div className="bar">
@@ -256,7 +257,7 @@ function WriteCard({ onChangeForm }: MyFormProps) {
         {countProgressBar === 3 ? (
           <div className="character">
             <div className="character__color-step3">
-              <img className="character__detail-step3" src={CharacterColor3} alt="" />
+              <img className="character__detail-step3" src={CharacterColor3Icon} alt="" />
               <div className="character__message">더 성장한 내일의 나를 위해!</div>
             </div>
             <div className="bar">
@@ -309,7 +310,7 @@ function WriteCard({ onChangeForm }: MyFormProps) {
 
         <div className="tag">
           해시태그
-          {/* <div className="tag__list">
+          <div className="tag__list">
             {interestList.map((interest, id) => {
               if (id < 5) {
                 return setInterestButton(id, interest);
@@ -321,7 +322,7 @@ function WriteCard({ onChangeForm }: MyFormProps) {
                 setIsOpenTag(!isOpenTag);
               }}
             >
-              <img className="tag__moreIcon" src={MoreIcon} alt=""></img>
+              <img className="tag__moreIcon" src={MoreClickedIcon} alt=""></img>
             </Button>
             <div className="tag__group">
               {isOpenTag === true ? (
@@ -336,7 +337,7 @@ function WriteCard({ onChangeForm }: MyFormProps) {
                 </button>
               ) : null}
             </div>
-          </div> */}
+          </div>
         </div>
 
         {countProgressBar === 3 ? (
@@ -363,189 +364,5 @@ function WriteCard({ onChangeForm }: MyFormProps) {
   );
 }
 
-const SWriteCard = Styled.div`
-    .header{
-      padding-top:100px;
-      text-align:center;
-      line-height: 1.22;
-      letter-spacing: -0.5px;
-      font-family: HomepageBaukasten;
-      font-size: 46px;
-      font-weight: bold;
-}
-    .challenge-card{
-        margin: 0 auto;
-        padding-top:60px;
-        width:844px;
-
-        &__title{
-            line-height:38px;
-            letter-spacing: -0.5px;
-            color: #3D3D3D;
-            font-size:22px;
-            font-weight: bold;
-        }
-        &__restriction{
-            padding-bottom: 30px;
-            text-align: right;
-            line-height: 18px;
-            color: #B1B1B1;
-            font-size: 14px;
-            font-weight: bold;
-        }
-    }
-    .textarea{
-        box-sizing: border-box;
-        align-items: center;
-        border: 1px solid #DFDFDF;
-        padding: 40px 60px 40px 60px;
-        width: 844px;
-        height:369px;
-        resize:none;
-        font-size: 18px;
-    }
-
-    .button{
-        align-items: center;
-        margin: 0 auto;
-        width:844px;
-    }
-    
-    .write__button-color{
-        margin: 0 auto;
-        margin-top:60px;
-        border:none;
-        border-radius: 4px;
-        background: linear-gradient(91.91deg, #36C8F5 7.34%, #13E2DD 90.35%);
-        width:844px;
-        height: 60px;
-        text-align: center;
-        color: #FFFFFF;
-    }
-    .write__button-black{
-        margin: 0 auto;
-        margin-top:60px;
-        border:none;
-        border-radius: 4px;
-        background: #DFDFDF;
-        width:844px;
-        height: 60px;
-        text-align: center;
-        color: #FFFFFF;
-    }
-    .character{
-      position:sticky;
-      top:20px;
-      margin: 0 auto;
-      background-color:white;
-      padding-top:60px;
-      width:844px;
-
-      &__black{
-        padding-top:29px;
-        padding-right:50px;
-    }
-
-      &__color-step1{
-          padding-top:29px;
-          padding-left:200px;
-      }
-      &__detail-step1{
-        padding-left:60px;
-      }
-
-      &__color-step2{
-          padding-left:450px;
-      }
-      &__detail-step2{
-        padding-left:50px;
-      }
-
-      &__color-step3{
-          text-align:end;
-      }
-        &__message{
-            padding-top:12.32px;
-            padding-bottom: 12px;
-            line-height: 1.43;
-            letter-spacing: -0.5px;
-            font-size: 14px;
-            font-weight: bold;
-        }
-    }   
-    .progressbar{
-        display: inline-block;
-        background-color: #d6d3d3;
-        width: 844px;
-        height: 5px;
-    }
-    .gauge__initial{
-        display: inline-block;
-        background-color: #3abff7;
-        width: 1%;
-        height: 5px;
-    }
-
-    .gauge__quarter{
-            display: inline-block;
-            background-color: #3abff7;
-            width: 34%;
-            height: 5px;
-        }
-    .gauge__half{
-            display: inline-block;
-            background-color: #3abff7;
-            width: 67%;
-            height: 5px;
-        }
-    .gauge__whole{
-            display: inline-block;
-            background-color: #3abff7;
-            width: 100%;
-            height: 5px;
-        }
-
-    .tag{
-      align-items:center;
-      margin:0 auto;
-      padding-top:60px;
-      width:844px;
-      text-align: left;
-      line-height: 1.33;
-      letter-spacing: -0.5px;
-      color: var(--colors-grayscale-6-f);
-      font-size: 24px;
-      font-weight: bold;
-
-      &__moreIcon{
-        padding-bottom:5px;
-        vertical-align : middle;
-      }
-  
-      &__interest{
-          margin : 12px 5px;
-          border: solid 1px #8b8b8b;
-          border-radius: 60px;
-          background-color:#FFFFFF;
-          padding : 12px 30px;
-          height : 48px;
-          line-height: 1.33;
-          letter-spacing: -0.5px;
-          color : #8b8b8b;
-          font-size: 18px;
-          font-weight: bold;
-        }
-        .tag__group{
-          padding-top:20px;
-        }
-        .tag__group-detail{
-          box-sizing: border-box;
-          border: 1px solid rgba(223, 223, 223, 0.5);
-          border-radius: 16px;
-          box-shadow: 0px 0px 15px rgba(23, 22, 91, 0.08);
-          background: #FFFFFF;
-          padding: 20px 30px 30px 30px;
-        }
-}`;
-
 export default WriteCard;
+
