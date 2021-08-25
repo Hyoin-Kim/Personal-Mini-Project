a09f19cf7fb49eba065d9831221f41b859a87aaf
Hyoin-kim <hi980506@naver.com>
2021-08-25 23:56:46 +0900

    Delete : 불필요한 코드 삭제

--- O2-Practice/src/components/molecules/ChallengeEditCard/index.tsx
+++ O2-Practice/src/components/molecules/ChallengeEditCard/index.tsx
@@ -44,37 +44,6 @@ type selectedStyleLists = {
   [key: string]: selectedStyle;
 };
 
-const selectedInterestStyle: selectedStyleLists = {
-  '건강 및 피트니스': { backgroundColor: '#6f6f6f', color: '#FFFFFF' },
-  '게임': { backgroundColor: '#6f6f6f', color: '#FFFFFF' },
-  '교육': { backgroundColor: '#6f6f6f', color: '#FFFFFF' },
-  '그래픽 및 디자인': { backgroundColor: '#6f6f6f', color: '#FFFFFF' },
-  '금융': { backgroundColor: '#6f6f6f', color: '#FFFFFF' },
-  'K-pop': { backgroundColor: '#6f6f6f', color: '#FFFFFF' },
-  '뉴스, 신문': { backgroundColor: '#6f6f6f', color: '#FFFFFF' },
-  '데이트': { backgroundColor: '#6f6f6f', color: '#FFFFFF' },
-  '날씨': { backgroundColor: '#6f6f6f', color: '#FFFFFF' },
-  '네비게이션': { backgroundColor: '#6f6f6f', color: '#FFFFFF' },
-  '출판,도서': { backgroundColor: '#6f6f6f', color: '#FFFFFF' },
-  '뷰티': { backgroundColor: '#6f6f6f', color: '#FFFFFF' },
-  '라이프 스타일': { backgroundColor: '#6f6f6f', color: '#FFFFFF' },
-  '만화': { backgroundColor: '#6f6f6f', color: '#FFFFFF' },
-  '부동산 / 홈 인테리어': { backgroundColor: '#6f6f6f', color: '#FFFFFF' },
-  '사진 및 비디오': { backgroundColor: '#6f6f6f', color: '#FFFFFF' },
-  '쇼핑': { backgroundColor: '#6f6f6f', color: '#FFFFFF' },
-  '비즈니스': { backgroundColor: '#6f6f6f', color: '#FFFFFF' },
-  '생산성': { backgroundColor: '#6f6f6f', color: '#FFFFFF' },
-  '소셜 네트워킹': { backgroundColor: '#6f6f6f', color: '#FFFFFF' },
-  '스포츠': { backgroundColor: '#6f6f6f', color: '#FFFFFF' },
-  '어린이': { backgroundColor: '#6f6f6f', color: '#FFFFFF' },
-  'AR앱': { backgroundColor: '#6f6f6f', color: '#FFFFFF' },
-  '여행': { backgroundColor: '#6f6f6f', color: '#FFFFFF' },
-  '유틸리티': { backgroundColor: '#6f6f6f', color: '#FFFFFF' },
-  '음식 및 음료': { backgroundColor: '#6f6f6f', color: '#FFFFFF' },
-  '음악': { backgroundColor: '#6f6f6f', color: '#FFFFFF' },
-  '의료': { backgroundColor: '#6f6f6f', color: '#FFFFFF' },
-  '기타': { backgroundColor: '#6f6f6f', color: '#FFFFFF' },
-};
 interface IEditCard {
   id: string;
 }
